ff631437c4e1af529e438e214f6400b3c1be9963
siddiqanshori <siddiq.anshori@gmail.com>
2023-12-02 22:51:49 +0700

    siddiq: update tampilan web ramalan cuaca

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hacktiv8 Ok');
+const search = document.querySelector('.search-box button');
+const weatherbox = document.querySelector('.data-weather-box');
+const contentbox = document.querySelector('.weather-content-box');
+const searchbox = document.querySelector('.search-box');
+
+search.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    contentbox.style.height = '400px';
+    weatherbox.style.visibility = 'visible';
+    // searchbox.style.alignItems = 'center';
+
+});
